9467a5f25ded7ade6bba5443ea67ba84b58742b7
Jesse Emerson <jesseemerson7@gmail.com>
2023-05-31 15:04:38 -0400

    fixed footer

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
 import "bootstrap/dist/css/bootstrap.css";
 import Header from "./components/header";
-import Footer from "./components/footer"
-import About from "./components/body/aboutMe"
-import Contact from "./components/body/Contact"
-import Portfolio from "./components/body/Portfolio"
-import Resume from "./components/body/Resume"
+import Footer from "./components/footer";
+import About from "./components/body/aboutMe";
+import Contact from "./components/body/Contact";
+import Portfolio from "./components/body/Portfolio";
+import Resume from "./components/body/Resume";
 
-import {useState} from "react"
+import { useState } from "react";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("about");
 
-
-  const [currentPage, setCurrentPage] = useState('about')
-
-
-  const renderPage = ()=>{
-    if (currentPage === 'about') {
+  const renderPage = () => {
+    if (currentPage === "about") {
       return <About />;
     }
-    if (currentPage === 'portfolio') {
+    if (currentPage === "portfolio") {
       return <Portfolio />;
     }
-    if (currentPage === 'contact') {
+    if (currentPage === "contact") {
       return <Contact />;
     }
     return <Resume />;
-  }
+  };
 
-  const handlePageChange = (page) => setCurrentPage(page)
+  const handlePageChange = (page) => setCurrentPage(page);
 
   return (
-    <div className="vw-100">
+    <>
+    <div className="min-vh-100">
       <Header currentPage={currentPage} handlePageChange={handlePageChange} />
       {renderPage()}
-      <Footer/>
     </div>
+    <Footer />
+    </>
   );
 }
 
