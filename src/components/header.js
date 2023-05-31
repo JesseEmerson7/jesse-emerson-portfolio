7c68432a87c99ee018aa9b0a67b78f4c67382bce
Jesse Emerson <jesseemerson7@gmail.com>
2023-05-31 18:00:23 -0400

    changing header to indicate what page user is on

--- src/components/header.js
+++ src/components/header.js
@@ -8,12 +8,12 @@ export default function header({handlePageChange}) {
         <h1>Jesse Emerson</h1>
         <p id="title-info" className="ps-md-4 text-md-start text-sm-center">FullStack Developer</p>
       </div>
-      <div id="navbar" className="col-sm-12 col-md ">
+      <div id="navbar" className="col-sm-12 col-md-7 ">
         <ul className="row">
-          <li className="col-lg col-md"><a href="#aboutMe"  onClick={() => handlePageChange('about')}>About Me</a></li>
-          <li className="col-lg col-md "><a href="#portfolio" onClick={() => handlePageChange('portfolio')}>Portfolio</a></li>
-          <li className="col-lg col-md"><a href="#contact" onClick={() => handlePageChange('contact')}>Contact Me</a></li>
-          <li className="col-lg col-md"><a href="#resume" onClick={() => handlePageChange('resume')}>Resume</a></li>
+          <li className="col-lg-2 col-md-2" onClick={() => handlePageChange('about')}><p>About Me</p></li>
+          <li className="col-lg-2 col-md-2"><a href="#portfolio" onClick={() => handlePageChange('portfolio')}>Portfolio</a></li>
+          <li className="col-lg-2 col-md-2"><a href="#contact" onClick={() => handlePageChange('contact')}>Contact Me</a></li>
+          <li className="col-lg-2 col-md-2"><a href="#resume" onClick={() => handlePageChange('resume')}>Resume</a></li>
         </ul>
       </div>
     </nav>
