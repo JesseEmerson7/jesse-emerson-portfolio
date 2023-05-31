2a236083e3224a33e8b0fdc6e747620f72b79ed9
Jesse Emerson <jesseemerson7@gmail.com>
2023-05-31 00:04:22 -0400

    got resume page up to date

--- src/components/body/Resume.js
+++ src/components/body/Resume.js
@@ -1,10 +1,26 @@
+import "../../assets/css/resume.css";
 
-
-export default function Resume(){
-    return(
-        <>
-        <iframe src="https://docs.google.com/document/d/e/2PACX-1vQm_Oaa6a0mRVJxxadB48gU6FTXk6clyunZnz-Nid8bG_n7gr8GGM-Qw_AeDKFf_mmyqTP7xInWOUOf/pub?embedded=true"></iframe>
-        <button className="btn btn-primary">Download Resume</button>
-        </>
-    )
+export default function Resume() {
+  const handleBtn = () => {
+    window.open(
+      "https://docs.google.com/document/d/19ORGKNzY3_gP4YXxHP__czSIeR66qwGkmT3Lw34UYVU/edit"
+    );
+  };
+  return (
+    <>
+      <div id="container">
+        <button
+          id="resume-btn"
+          onClick={handleBtn}
+          className="btn btn-primary col-2"
+        >
+          Download Resume
+        </button>
+        <iframe
+          className="resume-div col-10"
+          src="https://docs.google.com/document/d/e/2PACX-1vQm_Oaa6a0mRVJxxadB48gU6FTXk6clyunZnz-Nid8bG_n7gr8GGM-Qw_AeDKFf_mmyqTP7xInWOUOf/pub?embedded=true"
+        ></iframe>
+      </div>
+    </>
+  );
 }
