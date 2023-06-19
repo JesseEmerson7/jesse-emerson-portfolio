c029b37d2f51ca6ba62fe5a1e26b98d20ee12f86
Jesse Emerson <jesseemerson7@gmail.com>
2023-06-19 17:37:17 -0400

    changing contact page

--- src/components/body/Contact.js
+++ src/components/body/Contact.js
@@ -5,38 +5,9 @@ export default function Contact() {
 
   return (
     <>
-    <div id="form-container-div">
-      <div  className="form-group">
-        <label for="exampleInputEmail1">Full Name</label>
-        <input
-          id="name-input"
-          type="text"
-          className="form-control"
-          placeholder="Enter Name"
-        />
-      </div>
-      <div className="form-group">
-        <label>Email</label>
-        <input
-          id="Email-input"
-          type="text"
-          className="form-control"
-          placeholder="Email"
-        />
-      </div>
-      <div className="form-group">
-        <label>Message</label>
-        <input
-          id="message-input"
-          type="text"
-          className="form-control"
-          placeholder="Message"
-        />
-      </div>
-      <button id='submit-btn' className="btn btn-primary">
-        Submit
-      </button>
-      </div>
+    <section>
+      
+    </section>
     </>
   );
 }
