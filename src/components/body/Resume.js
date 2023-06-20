3c553272393082db8e68f4dfca5711d8023fc850
Jesse Emerson <jesseemerson7@gmail.com>
2023-06-20 17:01:33 -0400

    changed iframe

--- src/components/body/Resume.js
+++ src/components/body/Resume.js
@@ -3,23 +3,22 @@ import "../../assets/css/resume.css";
 export default function Resume() {
   const handleBtn = () => {
     window.open(
-      "https://docs.google.com/document/d/19ORGKNzY3_gP4YXxHP__czSIeR66qwGkmT3Lw34UYVU/edit"
+      "https://docs.google.com/document/d/13VHmJZDtbdPDRRBUbh3u9E-hdlLN-V7lI_g1xpTZvJs/edit?usp=sharing"
     );
   };
   return (
     <>
       <div id="container">
         <button
-          id="resume-btn"
           onClick={handleBtn}
-          className="btn btn-primary col-2"
+          className="btn btn-primary mt-5 rounded-b-none w-full md:w-3/6"
         >
-          Download Resume
+          Download via google docs
         </button>
         <iframe
-          className="resume-div col-10"
+          className=" w-full h-full md:w-3/6 mb-5"
           title="Jesse's Resume"
-          src="https://docs.google.com/document/d/e/2PACX-1vQm_Oaa6a0mRVJxxadB48gU6FTXk6clyunZnz-Nid8bG_n7gr8GGM-Qw_AeDKFf_mmyqTP7xInWOUOf/pub?embedded=true"
+          src="https://docs.google.com/document/d/e/2PACX-1vQU4AWKPLA2YzvPXCesPVB6ZIMTak1lycmkW5OpUF3MsshntNoPY19jGf5ZIwUUWDPdSv04uzx8pR1l/pub?embedded=true"
         ></iframe>
       </div>
     </>
