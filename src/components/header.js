2d1115b830ae1a69ddee668bdd621f38fd07330c
Jesse Emerson <jesseemerson7@gmail.com>
2023-07-21 00:58:42 -0400

    changed nav tabs with shadow and transitions

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,15 @@
 import "../assets/css/header.css";
-import 'animate.css';
+import "animate.css";
 
 export default function header({ currentPage, handlePageChange }) {
   return (
     <nav id="header" className="w-full flex-col flex flex-wrap md:flex-row">
       <div id="my-name" className="w-full my-5 md:w-1/3 rounded-sm">
         <h1 className=" text-5xl text-center w-full">Jesse Emerson</h1>
-        <p id="title-info" className="w-full text-center animate__animated animate__fadeIn">
+        <p
+          id="title-info"
+          className="w-full text-center animate__animated animate__fadeIn"
+        >
           Full Stack Developer
         </p>
       </div>
@@ -15,42 +18,42 @@ export default function header({ currentPage, handlePageChange }) {
           <li
             className={
               currentPage === "about"
-                ? "selected-tab md:w-2/6"
-                : "md:w-1/4"
+                ? "selected-tab md:w-2/6 transition-all"
+                : "md:w-1/4  transition-all"
             }
             onClick={() => handlePageChange("about")}
           >
-            <p>About</p>
+            <p className="mt-1">About</p>
           </li>
           <li
             className={
               currentPage === "portfolio"
-                ? "selected-tab md:w-2/6"
-                : 'md:w-1/4'
+                ? "selected-tab md:w-2/6 transition-all"
+                : "md:w-1/4 transition-all"
             }
             onClick={() => handlePageChange("portfolio")}
           >
-            <p>Portfolio</p>
+            <p className="mt-1">Portfolio</p>
           </li>
           <li
             className={
               currentPage === "contact"
-                ? "selected-tab md:w-2/6 "
-                : "md:w-1/4"
+                ? "selected-tab md:w-2/6 transition-all"
+                : "md:w-1/4 transition-all"
             }
             onClick={() => handlePageChange("contact")}
           >
-            <p>Contact</p>
+            <p className="mt-1">Contact</p>
           </li>
           <li
             className={
               currentPage === "resume"
-                ? "selected-tab md:w-2/6"
-                : "md:w-1/4"
+                ? "selected-tab md:w-2/6 transition-all"
+                : "md:w-1/4 transition-all"
             }
             onClick={() => handlePageChange("resume")}
           >
-            <p>Resume</p>
+            <p className="mt-1">Resume</p>
           </li>
         </ul>
       </div>
