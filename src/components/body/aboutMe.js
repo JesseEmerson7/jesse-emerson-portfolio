56de55955e63af538460ace249cbd87302dd56db
Jesse Emerson <jesseemerson7@gmail.com>
2023-09-16 12:37:57 -0400

    changed about me page html

--- src/components/body/aboutMe.js
+++ src/components/body/aboutMe.js
@@ -1,105 +1,96 @@
 import "../../assets/css/aboutMe.css";
-import profile2 from "../../assets/images/profile-5.jpg"
-import profile1 from "../../assets/images/profile-7.jpg"
-import profile3 from "../../assets/images/profile-8.jpg"
+import jax from "../../assets/images/jax.jpg";
 
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 export default function About() {
- //state for skill button text and picture slideshow
-  const [currentPic, ChangePic] = useState(0);
-  const [skillButton, changeSkill] = useState(false);
-
-  const pictureList = [profile1,  profile2, profile3 ];
-
-  useEffect(() => {
-    //slideshow starts on page load and loops after it reaches the end of the array
-    const interval = setInterval(() => {
-      ChangePic((prevIndex) => (prevIndex + 1) % pictureList.length);
-    }, 4000);
-
-    return () => clearInterval(interval);
-  }, [pictureList.length]);
-
-  const toggleSkill = () => {
-    changeSkill(!skillButton);
-    const container = document.getElementById("skill-scroll");
-    container.scrollIntoView({ behavior: "smooth", block: "center" });
-  };
-
   return (
     <>
-      <div id="about-me" className="w-full flex flex-col">
-        {/* about me section */}
-        <section className="flex flex-col md:flex-row md:mt-9">
-          <div
-            id="pic-container"
-            className="flex flex-col text-center md:w-1/3"
-          >
-            <img
-              id="profile-pic"
-              src={pictureList[currentPic]}
-              alt=" Profile pic"
-            />
-            <div id="picture-text">
-              <p className="text-xl">Jesse Emerson</p>
-              <p className=" text-lg">
-                MERN Stack Web Developer
-              </p>
-              <p></p>
+      {/* hero video will be bg of section */}
+      <section id="heroMp4">
+        <div>
+          {/* div housing the rotating logos - adjectives - learn more btn */}
+          <div>
+            {/* adjective text */}
+            <div>
+              <h2>Passionate</h2>
+              <button>Learn More</button>
             </div>
           </div>
-          <div
-            id="bio"
-            className=" w-[95%] mx-auto mb-5 md:w-2/3 md:h-2/3 md:my-auto md:mx-5"
-          >
-            {/* bio */}
-            <div id="bio-card" className="">
-              <div className="">
-                <p className="text-xl py-5 text-center">
-                  I am a full stack web developer certified by the University of
-                  Central Florida Coding Bootcamp Program. I am proficient in
-                  front-end technologies such as: HTML, CSS, and JavaScript.
-                  This allows me to create visually appealing and responsive
-                  user interfaces. With expertise in frameworks like React.js, I
-                  build unique, dynamic, and interactive web applications. On
-                  the back-end, I utilize Node.js and Express.js to develop
-                  RESTful APIs for data management. I excel at problem-solving,
-                  teamwork, and I always pay attention to detail. I'm excited to
-                  contribute to cutting-edge projects while continually
-                  enhancing my web development skills.
-                </p>
-              </div>
-            </div>
+        </div>
+        {/* font end back end and more buttons */}
+        <div>
+          {/* front */}
+          <div className="frontEnd">
+            <div className="LogoListDiv"></div>
+            <h3>Front End</h3>
           </div>
-        </section>
-        {/* skills section */}
-        <section className="flex flex-col flex-wrap my-10">
-          {/* daisy button start */}
-          <div
-            id="skill-scroll"
-            className={
-              skillButton
-                ? "collapse bg-base-200 text-amber-200 "
-                : "collapse bg-base-200 "
-            }
-            onClick={toggleSkill}
-          >
-            <input type="checkbox" />
-            <div className="collapse-title text-xl font-medium text-center">
-              {skillButton ? "Skills" : "Click to show skill list"}
-            </div>
-            <div className="collapse-content">
-              <marquee id="scroll-text">
-                JavaScript ES6+, CSS3, HTML5, SQL, NoSQL, GitHub, MongoDB,
-                Mongoose, MySQL, Sequelize, Express, React, Node, Handlebars,
-                jQuery, Bootstrap, Tailwind CSS
-              </marquee>
-            </div>
+          {/* back */}
+          <div className="backEnd">
+            <div className="LogoListDiv"></div>
+            <h3>Back End</h3>
           </div>
-          {/* end */}
-        </section>
-      </div>
+          {/* more */}
+          <div className="moreBtn">
+            <div className="LogoListDiv"></div>
+            <h3>More</h3>
+          </div>
+        </div>
+      </section>
+      {/* scrolling bg */}
+      <div id="scrollingBgAbout"></div>
+      <section>
+        {/* about me section div */}
+        <div>
+          {/* header div */}
+          <div>
+            <h4 className="aboutMeTitle">About me</h4>
+            <h4 className="educationTitle">Education</h4>
+            <h4 className="hobbiesTitle">Hobbies</h4>
+          </div>
+          {/* about me body - text to change on click of headers */}
+          <img alt="Jesse Emerson" />
+          <p className="aboutText">
+            I am a full stack web developer certified by the University of
+            Central Florida Coding Bootcamp Program. I am proficient in
+            front-end technologies such as: HTML, CSS, and JavaScript. This
+            allows me to create visually appealing and responsive user
+            interfaces. With expertise in frameworks like React.js, I build
+            unique, dynamic, and interactive web applications. On the back-end,
+            I utilize Node.js and Express.js to develop RESTful APIs for data
+            management. I excel at problem-solving, teamwork, and I always pay
+            attention to detail. I'm excited to contribute to cutting-edge
+            projects while continually enhancing my web development skills
+          </p>
+        </div>
+      </section>
+      <section>
+        {/* title of work experience */}
+        <div>
+          <h5>Work Experience</h5>
+        </div>
+        {/* cards for work */}
+        <div>
+          {/* card one for bees */}
+          <div>
+            {/* img */}
+            <p className="workTitle">Bee Keeper</p>
+            <p>info about this job</p>
+          </div>
+          {/* card about fire inspector */}
+          <div>
+            {/* img */}
+            <p>Fire Alarm Technician</p>
+            <p>info about this job</p>
+          </div>
+          {/* card about 1000 degrees */}
+          <div>
+            {/* img */}
+            <p>1000 Degrees Pizza</p>
+            <p>Info about this job</p>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
